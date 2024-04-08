fa1716ad92d8b2b66e8cf90fdeb25bdd20c27123
chenshane2013@gmail.com <sean>
2024-04-09 00:41:34 +0800

    add log

--- src/ubox/runes/rune_event_catcher.rs
+++ src/ubox/runes/rune_event_catcher.rs
@@ -6,6 +6,7 @@ use crate::index::entry::{Entry};
 use crate::{Result};
 use crate::index::{lot::Lot};
 use crate::ubox::runes::rune_event::{Etch, RuneBalance, RuneEvent, RuneEventOutput};
+use std::time::{Instant};
 
 pub(crate) struct RuneEventCatcher<'a, 'tx> {
   pub(crate) transaction_id_to_rune_event: &'a mut Table<'tx, &'static crate::index::entry::TxidValue, &'static [u8]>,
@@ -13,6 +14,7 @@ pub(crate) struct RuneEventCatcher<'a, 'tx> {
 
 impl RuneEventCatcher<'_, '_> {
   pub(crate) fn catch_event(&mut self, txid: Txid, tx: &Transaction, etch: Option<Etch>, burned: HashMap<RuneId, Lot>, allocated: Vec<HashMap<RuneId, Lot>>, inputs: Vec<RuneEventOutput>) -> Result<()> {
+    let start_time = Instant::now();
     let mut outputs: Vec<RuneEventOutput> = vec![];
     let mut burns: Vec<RuneBalance> = vec![];
     for (id, amount) in burned {
@@ -21,6 +23,8 @@ impl RuneEventCatcher<'_, '_> {
         balance: amount.0,
       })
     }
+    let burns_duration = start_time.elapsed();
+    println!("rune_event_catcher.burns_duration use time ：{:?}", burns_duration);
     for (vout, balances) in allocated.into_iter().enumerate() {
       if balances.is_empty() {
         continue;
@@ -41,10 +45,14 @@ impl RuneEventCatcher<'_, '_> {
         })
       }
     }
+    let allocated_duration = start_time.elapsed();
+    println!("rune_event_catcher.allocated_duration use time ：{:?}", allocated_duration);
     if !inputs.is_empty() || !outputs.is_empty() || !burns.is_empty() || etch.is_some() {
       let event = RuneEvent { txid, inputs, outputs, etch, burns };
       self.transaction_id_to_rune_event.insert(&txid.store(), rmp_serde::to_vec(&event).unwrap().as_slice())?;
     }
+    let save_duration = start_time.elapsed();
+    println!("rune_event_catcher.save_duration use time ：{:?}", save_duration);
     return Ok(());
   }
 }
